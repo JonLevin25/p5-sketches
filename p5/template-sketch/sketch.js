d5c55f8b778596d25fe94619c4f12f01b63b88a1
Jonathan Levin <jon.levin@inceptionxr.com>
2019-08-18 19:54:29 +0300

    library + template

---
+++ p5/template-sketch/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // put setup code here
+  createCanvas(800, 800)
+}
+
+function draw() {
+  // put drawing code here
+  background('green');
+  fill(128, 0, 0);
+  ellipse(width/2, height/2, 300);
+}
